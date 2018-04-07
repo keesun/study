97854bce46dab52a6cca1a6ba9910c03a416e357
Keesun Baik (a.k.a, Whiteship) <whiteship@epril.com>
2018-04-07 12:49:55 -0700

    Add docker-compose file for pgsql

--- jpa-getting-started/src/main/java/me/whiteship/jpasudy/Application.java
+++ jpa-getting-started/src/main/java/me/whiteship/jpasudy/Application.java
@@ -4,9 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class JpaSudyApplication {
+public class Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(JpaSudyApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
 }
