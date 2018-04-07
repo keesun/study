ccb3e7b24c9ba113dad16e10fc0b35786faf6f1a
Keesun Baik (a.k.a, Whiteship) <whiteship@epril.com>
2018-04-07 12:42:30 -0700

    Create new project for JPA Getting Started

---
+++ jpa-getting-started/src/main/java/me/whiteship/jpasudy/JpaSudyApplication.java
@@ -0,0 +1,12 @@
+package me.whiteship.jpasudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaSudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaSudyApplication.class, args);
+	}
+}
---
+++ jpa-getting-started/src/test/java/me/whiteship/jpasudy/JpaSudyApplicationTests.java
@@ -0,0 +1,16 @@
+package me.whiteship.jpasudy;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class JpaSudyApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
