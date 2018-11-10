0014fad4c5d07bbf6a985c8b03955d99539a3713
Keesun Baik (a.k.a, Whiteship) <whiteship@epril.com>
2018-11-10 12:19:39 +0900

    event CRUD

--- ksug201811restapi/src/main/java/me/whiteship/ksug201811restapi/events/EventController.java
+++ ksug201811restapi/src/main/java/me/whiteship/ksug201811restapi/events/EventController.java
@@ -3,19 +3,24 @@ package me.whiteship.ksug201811restapi.events;
 import me.whiteship.ksug201811restapi.common.ErrorResource;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PagedResourcesAssembler;
 import org.springframework.hateoas.Link;
+import org.springframework.hateoas.MediaTypes;
+import org.springframework.hateoas.PagedResources;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.Errors;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.net.URI;
+import java.util.Optional;
 
 import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
 
 @RestController
+@RequestMapping(value = "/api/events", produces = MediaTypes.HAL_JSON_UTF8_VALUE)
 public class EventController {
 
     @Autowired
@@ -27,7 +32,7 @@ public class EventController {
     @Autowired
     EventDtoValidator eventDtoValidator;
 
-    @PostMapping("/api/events")
+    @PostMapping
     public ResponseEntity create(@RequestBody @Valid EventDto eventDto,
                                  Errors errors) {
         if (errors.hasErrors()) {
@@ -50,5 +55,50 @@ public class EventController {
         eventResource.add(new Link("/docs/index.html#resources-events-create", "profile"));
         return ResponseEntity.created(uri).body(eventResource);
     }
+
+    @GetMapping
+    public ResponseEntity getEvents(Pageable pageable, PagedResourcesAssembler<Event> assembler) {
+        Page<Event> page = this.eventRepository.findAll(pageable);
+        PagedResources<EventResource> pagedResources = assembler.toResource(page, e -> new EventResource(e));
+        return ResponseEntity.ok(pagedResources);
+    }
+
+
+    @GetMapping("/{id}")
+    public ResponseEntity getEvent(@PathVariable Integer id) {
+        Optional<Event> byId = this.eventRepository.findById(id);
+        if (byId.isEmpty()) {
+            return ResponseEntity.notFound().build();
+        }
+
+        Event event = byId.get();
+        EventResource eventResource = new EventResource(event);
+        return ResponseEntity.ok(eventResource);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity updateEvent(@PathVariable Integer id,
+                                      @RequestBody @Valid EventDto eventDto,
+                                      Errors errors) {
+        if (errors.hasErrors()) {
+            return ResponseEntity.badRequest().body(new ErrorResource(errors));
+        }
+
+        eventDtoValidator.validate(eventDto, errors);
+        if (errors.hasErrors()) {
+            return ResponseEntity.badRequest().body(new ErrorResource(errors));
+        }
+
+        Optional<Event> byId = this.eventRepository.findById(id);
+        if (byId.isEmpty()) {
+            return ResponseEntity.notFound().build();
+        }
+
+        Event existingEvent = byId.get();
+        modelMapper.map(eventDto, existingEvent);
+        Event updatedEvent = this.eventRepository.save(existingEvent);
+        return ResponseEntity.ok(new EventResource(updatedEvent));
+    }
+
 
 }
--- ksug201811restapi/src/test/java/me/whiteship/ksug201811restapi/events/EventControllerTest.java
+++ ksug201811restapi/src/test/java/me/whiteship/ksug201811restapi/events/EventControllerTest.java
@@ -18,11 +18,13 @@ import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.time.LocalDateTime;
+import java.util.stream.IntStream;
 
 import static org.springframework.restdocs.hypermedia.HypermediaDocumentation.linkWithRel;
 import static org.springframework.restdocs.hypermedia.HypermediaDocumentation.links;
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
 import static org.springframework.restdocs.payload.PayloadDocumentation.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
@@ -40,6 +42,9 @@ public class EventControllerTest {
 
     @Autowired
     ObjectMapper objectMapper;
+
+    @Autowired
+    EventRepository eventRepository;
 
     @Test
     public void createEvent() throws Exception {
@@ -127,5 +132,44 @@ public class EventControllerTest {
                 .andExpect(jsonPath("$[0].objectName").hasJsonPath())
         ;
     }
+
+    @Test
+    public void getEvents() throws Exception {
+        // Given
+        IntStream.range(0, 30).forEach(this::saveEvent);
+
+        // When & Then
+        this.mockMvc.perform(get("/api/events")
+                    .param("size", "10")
+                    .param("page", "1"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("_links").hasJsonPath())
+        ;
+    }
+
+    private Event saveEvent(int index) {
+        Event event = Event.builder()
+                .name("test event" + index)
+                .build();
+        return this.eventRepository.save(event);
+    }
+
+    @Test
+    public void getEvent404() throws Exception {
+        this.mockMvc.perform(get("/api/events/1983"))
+                .andExpect(status().isNotFound());
+    }
+
+    @Test
+    public void getEvent() throws Exception {
+        Event event = this.saveEvent(100);
+
+        this.mockMvc.perform(get("/api/events/" + event.getId()))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("name").hasJsonPath())
+        ;
+    }
+
 
 }
