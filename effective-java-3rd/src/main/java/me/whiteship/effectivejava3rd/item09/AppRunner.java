cf8c6c5752f1240d39c5f4500bb6cc60e993b53f
Keesun Baik (a.k.a, Whiteship) <whiteship@epril.com>
2018-11-23 13:27:57 -0800

    project creation

--- effective-java-3rd/src/main/java/me/whiteship/effectivejava3rd/item09/AppRunner.java
+++ effective-java-3rd/src/main/java/me/whiteship/effectivejava3rd/item09/AppRunner.java
@@ -3,10 +3,14 @@ package me.whiteship.effectivejava3rd.item09;
 public class AppRunner {
 
     public static void main(String[] args) {
-        try(MyResource myResource = new MyResource();
-            MyResource myResource1 = new MyResource()) {
+        MyResource myResource = new MyResource();
+        try {
             myResource.doSomething();
-            myResource1.doSomething();
+        } catch (FirstError error) {
+            System.out.println("first error");
+            throw error;
+        } finally {
+            myResource.close();
         }
     }
 }
