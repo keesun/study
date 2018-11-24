ea6dc066e976c48e2b978f49fe1280b4f4a956ce
Keesun Baik (a.k.a, Whiteship) <whiteship@epril.com>
2018-11-23 16:24:35 -0800

    Add test descroption annotation

---
+++ rest-api-with-spring/src/main/java/me/whiteship/demoinfleanrestapi/common/TestDescription.java
@@ -0,0 +1,14 @@
+package me.whiteship.demoinfleanrestapi.common;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.SOURCE)
+public @interface TestDescription {
+
+    String value();
+
+}
--- rest-api-with-spring/src/test/java/me/whiteship/demoinfleanrestapi/events/EventControllerTests.java
+++ rest-api-with-spring/src/test/java/me/whiteship/demoinfleanrestapi/events/EventControllerTests.java
@@ -1,6 +1,7 @@
 package me.whiteship.demoinfleanrestapi.events;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import me.whiteship.demoinfleanrestapi.common.TestDescription;
 import org.hamcrest.Matchers;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -34,6 +35,7 @@ public class EventControllerTests {
     ObjectMapper objectMapper;
 
     @Test
+    @TestDescription("정상적으로 이벤트를 생성하는 테스트")
     public void createEvent() throws Exception {
         EventDto event = EventDto.builder()
                 .name("Spring")
@@ -64,6 +66,7 @@ public class EventControllerTests {
     }
 
     @Test
+    @TestDescription("입력 받을 수 없는 값을 사용한 경우에 에러가 발생하는 테스트")
     public void createEvent_Bad_Request() throws Exception {
         Event event = Event.builder()
                 .id(100)
@@ -92,6 +95,7 @@ public class EventControllerTests {
     }
 
     @Test
+    @TestDescription("입력 값이 비어있는 경우에 에러가 발생하는 테스트")
     public void createEvent_Bad_Request_Empty_Input() throws Exception {
         EventDto eventDto = EventDto.builder().build();
 
@@ -102,6 +106,7 @@ public class EventControllerTests {
     }
 
     @Test
+    @TestDescription("입력 값이 잘못된 경우에 에러가 발생하는 테스트")
     public void createEvent_Bad_Request_Wrong_Input() throws Exception {
         EventDto eventDto = EventDto.builder()
                 .name("Spring")
